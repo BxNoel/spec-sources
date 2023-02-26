91565313b29ac6cd60e375efa7df39f1a32878cb
aishani1604 <84450159+aishani1604@users.noreply.github.com>
2023-02-26 13:22:48 -0500

    react functionality

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,25 @@
 import logo from './Components/logo.png';
 import './App.css';
 import Form from './Components/Form';
-import Table from './Componets/Table';
+import React, {useState} from 'react';
+import Table from './Components/Table';
+
+const initialList = [{sourceName: 'Presbo', sourceEmail: 'presbo@columbia.edu'}, {sourceName: 'John Jay Mouse', sourceEmail: 'mouse@columbia.edu'}]
 
 function App() {
+  const [sourceList, setSourceList] = useState(initialList);
+  function handleAdd(sourceName, sourceEmail) {
+    const updatedList = [...sourceList, {sourceName: sourceName, sourceEmail: sourceEmail}]
+    setSourceList(updatedList);
+  }
   return (
     <div className="App">
-    <img src={logo} alt="logo" className='App-logo'/>
-    <div className="Header">
-      <h1>Spectator's Sources</h1>
-    </div>
-    
-    <Form></Form>
-    
-    {/* This is the Table */}
-    <Table/>
+        <img src={logo} alt="logo" className='App-logo'/>
+        <div class="Header">
+          <h1>Spectator's Sources</h1>
+        </div>
+        <Form handleAdd={handleAdd}></Form>
+        <Table/>
     </div>
   );
 }
