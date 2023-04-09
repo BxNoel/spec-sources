ffc46fb23fe9d9733af1c5c647c12ea88706ad38
aishani1604 <84450159+aishani1604@users.noreply.github.com>
2023-04-09 13:24:18 -0400

    Sources and Delete Source

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,14 +1,19 @@
+//import { sources } from '../data.js';
+const {Router} = require('express');
+const data = require('../data');
 module.exports = {
     hello: (req, res) => {
         res.json({"message": "Hello World!"});
     },
     sources: (req, res) => {
-        res.json({"message": "Retrieving"});
+        res.json({"message": data.sources});
     },
     add_source: (req, res) => {
         res.json({"message": "Adding"});
     },
     delete_source: (req, res) => {
-        res.json({"message": "Deleting"});
+        r = data.sources.filter(source => source.id != req.params.id);
+        data.sources = r;
+        res.json({"message": data.sources});
     },
 };
